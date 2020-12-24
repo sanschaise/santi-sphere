d7c81540627b9ad050e920123ef36e8ebffa8e46
Pedro Sanches <p.gomez.almeida@gmail.com>
2020-12-24 16:25:00 -0300

    grain

--- src/index.js
+++ src/index.js
@@ -111,7 +111,7 @@ function SetupGUI() {
 function SetupScene() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color("rgb(255, 255, 255)");
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 35;
     camera.position.x = 0;
     renderer = new THREE.WebGLRenderer({ antialias: true });
